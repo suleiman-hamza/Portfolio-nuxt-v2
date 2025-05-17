cc9de6150f7679f511ad66b17d1b38ffeec7fb99
suleiman hamza <hamzasuleiman530@gmail.com>
2025-05-17 14:39:45 +0100

    [package]: added nuxt/tailwind module

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -6,7 +6,7 @@
                 <h1>Suleiman Hamza</h1>
                 <p>~ Hunter</p>
             </span>
-            <h2>Software Engineer based in Abuja</h2>
+            <h2 class="bg-blue-500">Software Engineer based in Abuja</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>

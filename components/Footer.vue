095660490eee2987420af1ba249233786e2ae367
suleiman hamza <hamzasuleiman530@gmail.com>
2025-05-18 21:22:29 +0100

    [layout]: footer structure

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer class="">
+        <section class="flex item-center justify-center border-bottom">
+            <span class=" block icon border w-[40px] h-[40px]"></span>
+            <span class=" block icon border w-[40px] h-[40px]"></span>
+            <span class=" block icon border w-[40px] h-[40px]"></span>
+        </section>
+        <div class="p-4"><p class="text-center">If you love this, give me a star on github :)</p></div>
+    </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
     <Header />
         <slot></slot>
-    <p>The Footer here</p>
+    <Footer />
 </template>

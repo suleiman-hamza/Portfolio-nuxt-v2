d404e9ebd6eeb270b0d60796de4ac6f93e74aec3
suleiman hamza <hamzasuleiman530@gmail.com>
2025-05-18 06:45:09 +0100

    [hotfix]: remove border-x on black bg section

--- components/Projects.vue
+++ components/Projects.vue
@@ -3,7 +3,7 @@
 
 <template>
     <section class="border-bottom">
-        <div class="p-4 border-x border-[#dedede] container bg-black text-white">
+        <div class="p-4 container bg-black text-white">
             <p class="text-[#dedede]">Project Section</p>
             <p>Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
         </div>

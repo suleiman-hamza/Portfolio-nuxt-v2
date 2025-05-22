77c25b34c02082be6b6454768c35e46d662a07b2
suleiman hamza <hamzasuleiman530@gmail.com>
2025-05-22 07:14:42 +0100

    [wip]: hamburger menu with sheets to the top, working alignment

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,9 +4,9 @@
 <template>
     <footer class="">
         <section class="flex item-center justify-center border-bottom">
-            <span class=" block icon border w-[40px] h-[40px]"></span>
-            <span class=" block icon border w-[40px] h-[40px]"></span>
-            <span class=" block icon border w-[40px] h-[40px]"></span>
+            <span class="block icon border w-[40px] h-[40px]"></span>
+            <span class="block icon border w-[40px] h-[40px]"></span>
+            <span class="block icon border w-[40px] h-[40px] bg-background"></span>
         </section>
         <div class="p-4"><p class="text-center">If you love this, give me a star on github :)</p></div>
     </footer>
--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
-const count = ref(1)
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import { Menu } from 'lucide-vue-next';
+
+const isOpen = ref(false)
 </script>
 <template>
     <section class="border-bottom">
-        <nav class="flex justify-between py-[17px] px-[24px] border-x border-[#dedede] container">
-            <span class="logo">
-                <h2>SH</h2>
+        <nav class="flex justify-between items-center p-4 border-x border-[#dedede] container">
+            <span class="logo size-fit">
+                <h2 class="leading-[1] border text-[1.4rem]">SH</h2>
             </span>
+
+            <div class="size-fit">
+                <Sheet>
+                    <SheetTrigger class="size-fit"><Menu /></SheetTrigger>
+                    <SheetContent class="w-full" side="top">
+                    <SheetHeader>
+                        <SheetTitle>Are you absolutely sure?</SheetTitle>
+                        <SheetDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                        </SheetDescription>
+                    </SheetHeader>
+                    </SheetContent>
+                </Sheet>
+            </div>
             
-            <ul class="flex gap-4">
+            <ul class="hidden lg:flex gap-4">
                 <li class="bg-blue-400"><NuxtLink to="/">Home</NuxtLink></li>
                 <li><NuxtLink to="/">About</NuxtLink></li>
                 <li><NuxtLink to="/">Work</NuxtLink></li>

a70b14ca40f9b672e3863df8427ac42974c919cb
suleiman hamza <hamzasuleiman530@gmail.com>
2025-05-28 15:51:59 +0100

    [refactor]: added footer social media icons, changed stroke width of svg logo, created blog psge & it's index file.

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,10 +4,22 @@
 <template>
     <footer class="">
         <section class="flex item-center justify-center border-bottom">
-            <span class="block icon border w-[40px] h-[40px] bg-green-200">G</span>
-            <span class="block icon border w-[40px] h-[40px] bg-pink-300">T</span>
-            <span class="block icon border w-[40px] h-[40px] bg-blue-400">In</span>
-            <span class="block icon border w-[40px] h-[40px] bg-blue-500">BS</span>
+            <!--x-logo-->
+            <span class="flex items-center justify-center icon border w-[40px] h-[40px] bg-green-200 p-2">
+                <img src="../assets/icons/platform-x-icon.svg" alt="X ICONSVG"/>
+            </span>
+            <!--instagram-logo-->
+            <span class="block icon border w-[40px] h-[40px] bg-pink-300 p-2">
+                <img src="../assets/icons/platform-Instagram-icon.svg" alt="instagram"/>
+            </span>
+            <!--discord-logo-->
+            <span class="block icon border w-[40px] h-[40px] bg-blue-400 p-2">
+                <img src="../assets/icons/platfrom-discord-icon.svg" alt="discord logo"/>
+            </span>
+            <!--bluesky-logo-->
+            <span class="block icon border w-[40px] h-[40px] bg-blue-500 p-2">
+                <img src="../assets/icons/platform-x-icon.svg" alt="Bluesky butterfly"/>
+            </span>
         </section>
         <div class="p-4"><p class="text-center">If you love this, give me a star on github :)</p></div>
     </footer>
---
+++ pages/blog/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+<main class="border-pink-600 border-2">
+    <div><p>This is my blog</p></div>
+</main>
+</template>

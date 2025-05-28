8d96039b0326146e01c5184fe74157cfc8c6341d
suleiman hamza <hamzasuleiman530@gmail.com>
2025-05-28 06:03:26 +0100

    [featuer]: installed motion-v for motion & animation, tested and works

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import { motion } from 'motion-v';
 
 function runfunc() {
     console.log('something dey happen')
@@ -8,11 +8,16 @@ function runfunc() {
 
 <template>
     <section class="border-bottom">
-        <div class="p-4 container bg-black text-white"> <!--border-x border-[#dedede]-->
+        <div class="p-4 container bg-(--secondary-foreground) text-white"> <!--border-x border-[#dedede]-->
             <p class="text-[#dedede] mb-4">Project Section</p>
             <p class="mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <Buttonx @click="runfunc" class="bg-teal-600 !text-[#202020]">All Projects</Buttonx>
             <!-- <Button @click="runfunc">Button</Button> -->
+            <motion.div 
+                class="w-20 h-20 bg-blue-600 rounded-md mt-4"
+                :initial="{ rotate: 0 }"
+                :hover="{ rotate: 360 }"
+            />
         </div>
     </section>
 </template>

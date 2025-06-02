616a24e0d6c4a92f2f5b780da8bb5d306ce93c9b
suleiman hamza <hamzasuleiman530@gmail.com>
2025-06-02 02:54:46 +0100

    used crossed div in background component

--- components/Background.vue
+++ components/Background.vue
@@ -4,7 +4,7 @@
 <template>
     <section class="border-bottom">
         <div class="p-4 container bg-(--secondary-foreground) text-white"> <!--border-x border-[#dedede]-->
-            <p class="text-[#dedede] !mb-4 size-min border">Background</p>
+            <!-- <p class="text-[#dedede] !mb-4 size-min border">Background</p> -->
             <CrossedDiv class="text-[#dedede] !mb-4 size-min p-2">Background</CrossedDiv>
             <p class="!mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <p class="!mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
--- components/Projects.vue
+++ components/Projects.vue
@@ -9,7 +9,8 @@ function runfunc() {
 <template>
     <section class="border-bottom">
         <div class="p-4 container grid-background border-x border-[#dedede]"> <!--border-x border-[#dedede]-->
-            <p class="text-[#dedede] !mb-4">Project Section</p>
+            <!-- <p class="text-[#dedede] !mb-4">Project Section</p> -->
+            <CrossedDiv class="text-[#dedede] !mb-4 size-min p-2">Projects</CrossedDiv>
             <p class="!mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <Buttonx @click="runfunc" class="">All Projects</Buttonx>
         </div>

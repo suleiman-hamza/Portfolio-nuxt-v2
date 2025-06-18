6619f124d078ff6b086c783b32f6bbf6cfa6b081
suleiman hamza <hamzasuleiman530@gmail.com>
2025-06-18 06:52:06 +0100

    [refactor]: fully customizable crossedDiv component

--- components/Background.vue
+++ components/Background.vue
@@ -5,7 +5,7 @@
     <section class="border-bottom">
         <div class="p-4 container bg-(--secondary-foreground) text-white"> <!--border-x border-[#dedede]-->
             <!-- <p class="text-[#dedede] !mb-4 size-min border">Background</p> -->
-            <CrossedDiv class="text-[#dedede] !mb-4 size-min p-2" crossed="bg-[#dedede]">Background</CrossedDiv>
+            <CrossedDiv class="text-[#dedede] !mb-4 size-min p-2" crossx="before:bg-white" crossy="after:bg-white">Background</CrossedDiv>
             <p class="!mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <p class="!mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <p class="!mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>

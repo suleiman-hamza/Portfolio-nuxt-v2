7bc65bc2c56142648269db8e02c7804902d1e705
suleiman hamza <hamzasuleiman530@gmail.com>
2025-06-18 07:19:33 +0100

    [feature]: project cards in overscroll

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -3,12 +3,12 @@ import { Anvil } from 'lucide-vue-next';
 </script>
 <template>
     <section class="border-bottom">
-        <main class="container p-4 border-x border-[#dedede] grid-background">
-            <span class="flex items-center">
+        <main class="container p-4 sm:p-8 border-x border-[#dedede] grid-background">
+            <span class="flex items-center mb-4">
                 <h1 class="mb-4">Suleiman Hamza</h1>
                 <p>~ Hunter</p>
             </span>
-            <h2 class="font-bold text-2xl">Suleiman, a creative developer pursuing standout solutions that are meaningful & scalable.</h2>
+            <h2 class="font-bold text-2xl md:text-4xl !mb-4">Suleiman, a creative developer pursuing standout solutions that are meaningful & scalable.</h2>
             <p>I strive to create intuitive ideas that upscale products via my design thinking approach. I am always exploring new opportunities to improve user experience across various industries. Feel free to explore my work and connect with me, I am excited to hear from you!</p>
         </main>
     </section>
--- components/Projects.vue
+++ components/Projects.vue
@@ -8,8 +8,16 @@ function runfunc() {
 <template>
     <section class="border-bottom">
         <div class="p-4 container border-x border-[#dedede]"> <!--border-x border-[#dedede]-->
-            <!-- <p class="text-[#dedede] !mb-4">Project Section</p> -->
             <CrossedDiv class="size-min p-1 mb-4" as="section">Projects</CrossedDiv>
+            <!--selected project cards-->
+            <div class="mb-4 flex gap-4 overflow-x-scroll w-full flex-initial">
+                <div class="w-[150px] h-[200px] border flex-none"></div>
+                <div class="w-[150px] h-[200px] border flex-none"></div>
+                <div class="w-[150px] h-[200px] border flex-none"></div>
+                <div class="w-[150px] h-[200px] border flex-none"></div>
+                <div class="w-[150px] h-[200px] border flex-none"></div>
+                <div class="w-[150px] h-[200px] border flex-none"></div>
+            </div>
             <p class="!mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <Buttonx @click="runfunc" class="">All Projects</Buttonx>
         </div>

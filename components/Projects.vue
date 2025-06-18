c1da3db1e982efc9509dcc5d77ec6009ac0af804
suleiman hamza <hamzasuleiman530@gmail.com>
2025-06-18 14:41:59 +0100

    [wip]: project list gradients

--- components/Projects.vue
+++ components/Projects.vue
@@ -11,12 +11,32 @@ function runfunc() {
             <CrossedDiv class="size-min p-1 mb-4" as="section">Projects</CrossedDiv>
             <!--selected project cards-->
             <div class="mb-4 flex gap-4 overflow-x-scroll w-full flex-initial">
-                <div class="w-[150px] h-[200px] border flex-none"></div>
-                <div class="w-[150px] h-[200px] border flex-none"></div>
-                <div class="w-[150px] h-[200px] border flex-none"></div>
-                <div class="w-[150px] h-[200px] border flex-none"></div>
-                <div class="w-[150px] h-[200px] border flex-none"></div>
-                <div class="w-[150px] h-[200px] border flex-none"></div>
+                <div class="w-[150px] h-[200px] border flex-none bg-linear-to-r from-indigo-400/50 via-purple-400 to-pink-400/50">
+                    <div class="tab w-full border h-full p-4">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="w-[150px] h-[200px] border flex-none bg-linear-to-b from-red-400 via-orange-400 to-amber-700/60">
+                    <div class="tab w-full border h-full p-4">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="w-[150px] h-[200px] border flex-none bg-linear-to-r from-green-300 via-green-500 to-green-900/50">
+                    <div class="tab w-full border h-full p-4">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="w-[150px] h-[200px] border flex-none bg-linear-to-b from-sky-400/50 via-blue-400 to-blue-900/80">
+                    <div class="tab w-full h-full p-4">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, cumque. eque volupt</p>
+                    </div>
+                </div>
+                <div class="w-[150px] h-[200px] border flex-none bg-linear-to-b from-slate-400 via-rose-200 to-pink-400/50">
+                    <div class="tab w-full h-full p-4">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, cumque. eque volupt</p>
+                    </div>
+                </div>
+                <div class="w-[150px] h-[200px] border flex-none bg-linear-to-r from-yellow-400/50 via-purple-400 to-fuchsia-400/50"></div>
             </div>
             <p class="!mb-4">Lorem ipsum dolor sit amet . Animi eveniet pariatur nulla temporibus veritatis deleniti tempore laborum officia laboriosam minus suscipit similique, porro cum consectetur, doloremque dicta excepturi iste provident.</p>
             <Buttonx @click="runfunc" class="">All Projects</Buttonx>
@@ -25,4 +45,8 @@ function runfunc() {
 </template>
 
 <style scoped>
+.tab {
+    background-color: transparent;
+    backdrop-filter: blur(30px);
+}
 </style>

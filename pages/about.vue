8200a6436516d136ec514cfea239c35cbb3c37a4
suleiman hamza <hamzasuleiman530@gmail.com>
2025-07-04 10:39:21 +0100

    [refactor]: organize about page sections

--- pages/about.vue
+++ pages/about.vue
@@ -2,16 +2,52 @@
 
 </script>
 <template>
-    <main class="p-4">
-        <h1></h1>
-        <h1>More About me</h1>
-        <section>
-            <h3>Background</h3>
+    <main class="border-b">
+        <section class="container border-x border-[#dedede]">
+            <header class="mb-4 pt-16 p-4">
+                <h1 class="text-2xl font-semi !mb-4">About</h1>
+                <h2 class="!mb-2">Hey, I'm Suleiman</h2>
+                <img src="#" alt="My pic">
+            </header>
+            <section class="p-4">
+            <h3 class="text-2xl font-semi !mb-4">Intro...</h3>
             <p>I have been working with Frontend technologies for over 2 Years. During these years i have been able to work with designers to create delightful User Interface and debug web apps.</p>
             <p>My interest in the web started back in 2022. I was always curious about everything Tech & Trends. After trying out diffrent career paths in tech - from cybersecurity to penetration testing to programming, it just felt right the moment i began to write code.</p>
             <p>I’m a Frontend Engineer with a passion for crafting smooth, responsive web experiences using Vue.js/Nuxt, Node.js/Express and TypeScript. My expertise lies in building intuitive interfaces and robust applications, leveraging powerful component libraries like VueUse and Vue Motion to create reusable and efficient code.</p>
             <p>I’m well-versed in the Vue ecosystem, with a deep understanding of tools like Pinia, Vue Router, PrimeVue, Vitest, Nuxt-UI and Vite. My goal is to build applications that are not only visually engaging but also performant and scalable. i'm constantly exploring the latest advancements in frontend technology.</p>
             <p>My other interests includes: Mountain Biking, Basketball, Events or Meetups, Volunteering and Gaming.</p>
+            </section>
+
+            <span class="grid-background block h-[50px]"></span>
+            
+            <section class="p-4">
+                <h2 class="text-2xl font-semi !mb-4">Experience</h2>
+                <div class="mb-2">
+                    <h3 class="font-bold">Frontend Developer</h3>
+                    <h4>2024 - Present / RovelTech</h4>
+                </div>
+                <div class="mb-2">
+                    <h3 class="font-bold">Frontend Developer</h3>
+                    <h4>2024 - Present / RovelTech</h4>
+                </div>
+                <div class="mb-2">
+                    <h3 class="font-bold">Frontend Developer</h3>
+                    <h4>2024 - Present / RovelTech</h4>
+                </div>
+            </section>
+
+            <span class="grid-background block h-[50px]"></span>
+
+            <section class="p-4">
+                <h2 class="text-2xl font-semi !mb-4">Stack</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia totam quos numquam ipsam a ipsum vel. Odio quaerat sunt ea at dolorum, ad aliquid, omnis nemo sint doloremque corporis quod?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia totam quos numquam ipsam a ipsum vel. Odio quaerat sunt ea at dolorum, ad aliquid, omnis nemo sint doloremque corporis quod?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia totam quos numquam ipsam a ipsum vel. Odio quaerat sunt ea at dolorum, ad aliquid, omnis nemo sint doloremque corporis quod?</p>
+            </section>
         </section>
     </main>
 </template>
+
+<style lang="css" scoped>
+
+</style>

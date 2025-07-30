8375bd59e0a4b4f4abe9eed83fca57fc0812a912
suleiman hamza <hamzasuleiman530@gmail.com>
2025-07-30 22:01:20 +0100

    style: carousel laid out with clean minimal design

--- app/components/Clients.vue
+++ app/components/Clients.vue
@@ -11,47 +11,54 @@ const testimonyList = ref([
     {
         client: "ABdulRauf-Star",
         text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eaque mollitia quidem veritatis perspiciatis, ducimus, iusto ad nostrum velit atque at ullam incidunt possimus ut in illum. Soluta, repellendus dignissimos.",
+        role: "CTO, Readyl."
     },
     {
-        client: "Chaliph Joy",
+        client: "Ayatullah Suleiman",
         text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eaque mollitia quidem veritatis perspiciatis, ducimus, iusto ad nostrum velit atque at ullam incidunt possimus ut in illum. Soluta, repellendus dignissimos.",
+        role: "Cactus Jack Wears"
     },
     {
         client: "Adunniade rose",
         text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eaque mollitia quidem veritatis perspiciatis, ducimus, iusto ad nostrum velit atque at ullam incidunt possimus ut in illum. Soluta, repellendus dignissimos.",
+        role: "Designer"
     },
     {
         client: "Toluwase Bewaji",
         text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eaque mollitia quidem veritatis perspiciatis, ducimus, iusto ad nostrum velit atque at ullam incidunt possimus ut in illum. Soluta, repellendus dignissimos.",
+        role: "Mobile App Developer"
     },
     {
         client: "Rovel Boss",
-        text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eaque mollitia quidem veritatis perspiciatis, ducimus, iusto ad nostrum velit atque at ullam incidunt possimus ut in illum. Soluta, repellendus dignissimos.", 
+        text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eaque mollitia quidem veritatis perspiciatis, ducimus, iusto ad nostrum velit atque at ullam incidunt possimus ut in illum. Soluta, repellendus dignissimos.",
+        role: "Designer | CEO, Rovel Tech"
     }
 ])
 </script>
 <template>
     <section class="border-bottom">
-        <div class="p-4 py-10 container border-x border-[#dedede]"> <!--border-x border-[#dedede]-->
+        <div class="p-6 py-10 container border-x border-[#dedede]"> <!--border-x border-[#dedede]-->
             <CrossedDiv class="size-max text-[1.2rem] p-1 mb-8">Clients Tesimonials</CrossedDiv>
-            <h2>Clients Tesimonials</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eaque mollitia quidem veritatis
-                perspiciatis, ducimus, iusto ad nostrum velit atque at ullam incidunt possimus ut in illum. Soluta,
-                repellendus dignissimos.</p>
-            <section>
-                <Carousel class="relative w-full">
-                    <CarouselContent>
-                        <CarouselItem v-for="item in testimonyList">
-                            <div class="flex flex-col items-center justify-center gap-4 p-4 border rounded-lg border-red-200">
-                                <h2>{{ item.client }}</h2>
-                                <p class="border p-2">{{ item.text }}</p>
+        <p class="text-center text-2xl !mb-6">Here's what 99% of my clients say about my work</p>
+        <section class="mb-12">
+            <Carousel class="relative w-full">
+                <CarouselContent>
+                    <CarouselItem v-for="item in testimonyList" class="sm:basis-1/2 md:basis-1/3">
+                        <div class="flex flex-col items-center justify-center gap-4 p-4 border bg-red-200 rounded-lg border-red-200">
+                            <p class="border p-2">{{ item.text }}</p>
+                            <div class="border flex flex-col justify-center items-center gap-1">
+                                <span class="block w-[40px] h-[40px] border rounded-full"></span>
+                                <h2 class="text-md">{{ item.client }}</h2>
+                                <h3 class="text-(--muted-foreground) text-sm">{{ item.role }}</h3>
                             </div>
-                        </CarouselItem>
-                    </CarouselContent>
-                    <CarouselPrevious class="top-0 left-0"/>
-                    <CarouselNext class="top-0 right-0" />
-                </Carousel>
-            </section>
-        </div>
+                        </div>
+                    </CarouselItem>
+                </CarouselContent>
+                <CarouselPrevious class="top-[50%] left-[-15px]"/>
+                <CarouselNext class="top-[50%] right-[-15px]" />
+            </Carousel>
+        </section>
+        <p class="text-center text-2xl !mb-6">Here's what 99% of my clients say about my work</p>
+    </div>
     </section>
 </template>

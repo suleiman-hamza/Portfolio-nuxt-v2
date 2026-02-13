05792760cbe891a8d84d35cd88eebc7d867c2dcd
suleiman hamza <hamzasuleiman530@gmail.com>
2026-02-13 16:01:35 +0100

    feat: svg bg pattern on slider

--- app/components/Sidebar.vue
+++ app/components/Sidebar.vue
@@ -12,31 +12,40 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
+import { useRoute } from 'vue-router';
+const route = useRoute()
+
+const closeSheet = ref(false)
+watch(route, () => {
+    closeSheet.value = false
+})
 </script>
 
 <template>
-  <Sheet>
+  <Sheet v-model:open="closeSheet">
     <SheetTrigger as-child>
       <Button variant="outline" size="icon" class="p-2 rounded-none">
         <Menu />
       </Button>
     </SheetTrigger>
     <SheetContent>
-      <SheetHeader class="border">
-        <SheetClose class="border size-fit ml-auto"><OctagonX /></SheetClose>
-        <SheetTitle>profile</SheetTitle>
-        <SheetDescription>
-          Make changes to your profile here. Click save when you're done.
-        </SheetDescription>
+      <SheetHeader class="p-0 border relative">
+        <NuxtImg src="/images/afro-pattern.svg" class="w-full" />
+        <SheetClose class="border size-fit ml-auto absolute top-4 right-4 z-10">
+            <OctagonX />
+        </SheetClose>
       </SheetHeader>
       <div class="p-4 text-white">
-        <p>Some Content</p>
         <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/about">PlayGround</NuxtLink>
+        <NuxtLink to="/about">Github</NuxtLink>
+        <NuxtLink to="/about">Project</NuxtLink>
       </div>
-      <SheetFooter class="border">
-        <Button type="submit">
+      <SheetFooter class="border p-0">
+        <NuxtImg src="/images/afro-pattern.svg" class="w-full" />
+        <!-- <Button type="submit">
           Save changes
-        </Button>
+        </Button> -->
         <!-- <SheetClose as-child>
           <Button variant="outline">
             Close

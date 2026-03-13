b3d09a3d973d233d3b6f9f1ccd23fd3e05434c24
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-13 19:57:02 +0100

    chore: new odoo4dev page

---
+++ app/pages/odoo4dev.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'default'
+})
+</script>
+
+<template>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente consectetur tempora porro recusandae repellat cumque magnam labore, quia aperiam ad eius magni dolore aliquid et sint voluptatibus. Tempore, tenetur voluptatibus.</p>
+    </section>
+</template>

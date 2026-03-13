879f305fea1c7b67be804624563f1f5661e99e60
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-13 22:55:19 +0100

    chore: odoo page specification

--- app/pages/odoo4dev.vue
+++ app/pages/odoo4dev.vue
@@ -5,7 +5,46 @@ definePageMeta({
 </script>
 
 <template>
-    <section>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente consectetur tempora porro recusandae repellat cumque magnam labore, quia aperiam ad eius magni dolore aliquid et sint voluptatibus. Tempore, tenetur voluptatibus.</p>
+    <section class="mt-(--ui-header) p-4">
+        <h1 class="text-muted text-2xl mb-4">Odoo Development Service (v18, v19+)</h1>
+        <p>I provide full cycle odoo implementation, from requirement gathering to deployment with minimal disruption. By aligning odoo with your business goals, i craft ERP Solutions that align perfectly with your workflow, compliance needs and growth goals across multiple industries.</p>
+        <div>
+            <h2>Technologies used for Odoo Development</h2>
+            <div class="flex gap-4 flex-wrap">
+                <div class="border">
+                    <h4>Frontend</h4>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                    </ul>
+                </div>
+                <div class="border">
+                    <h4>Frontend</h4>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                    </ul>
+                </div>
+                <div class="border">
+                    <h4>Backend</h4>
+                    <ul>
+                        <li>Python</li>
+                        <li>XML</li>
+                        <li>Javascript</li>
+                    </ul>
+                </div>
+                <div class="border">
+                    <h4>Database</h4>
+                    <ul>
+                        <li>Postgres</li>
+                        <li>Docker</li>
+                        <li>PGadmin 4</li>
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
     </section>
 </template>

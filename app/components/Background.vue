d6a872fcf6146cb61c77f39e013ae45de01dd263
suleiman hamza <hamzasuleiman530@gmail.com>
2026-03-14 12:30:15 +0100

    feat/wip: background bg-pattern img

--- app/components/Background.vue
+++ app/components/Background.vue
@@ -1,24 +1,55 @@
 <script setup lang="ts">
+import { motion } from 'motion-v';
 import { ArrowUpRight } from 'lucide-vue-next'
+
+const draw = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: (i: number) => {
+        const delay = i * 0.5
+        return {
+            pathLength: 1,
+            opacity: 1,
+            transition: {
+                pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+                opacity: { delay, duration: 0.01 },
+            },
+        }
+    },
+}
+
+const image = {
+    maxWidth: "80vw",
+}
+
+const shape = {
+    strokeWidth: 10,
+    strokeLinecap: "round",
+    fill: "transparent",
+}
 </script>
 
 <template>
     <section class="border-b border-gray-300/50">
         <div class="grid gap-8 sm:grid-cols-2 p-4 py-10 container border-gray-300/50 bg-foreground border-x">
-            <div class="border min-h-75"></div>
+            <div class="min-h-75 bg-cover bg-center bg-[url(/images/8pattern-bg.png)]">
+
+            </div>
             <div class="max-w-2xl">
-                    <CrossedDiv class="align-center mx-auto size-max p-2 mb-4">
-                        Background
-                    </CrossedDiv>
-                    <p class="text-md text-base sm:text-xl text-pretty leading-8">Hello, My name is Suleiman Hamza Suleiman (shs). I am a detail-oriented, design-driven and passionate developer. I thrive in building scalable web applications using modern frontend technologies, integrating complex systems and leveragings modern development tools and practices.</p>
-                    <NuxtLink to="/about" class="text-sky-500 underline flex gap-1 items-center justify-center size-fit leading-0 transition-all duration-200 mt-4">
-                        <span class="">More About Me</span>
-                        <ArrowUpRight :size="16" />
-                    </NuxtLink>
-                </div>
+                <CrossedDiv class="align-center mx-auto size-max p-2 mb-4">
+                    Background
+                </CrossedDiv>
+                <p class="text-md text-base sm:text-xl text-pretty leading-8">Hello, My name is Suleiman Hamza Suleiman
+                    (shs). I am a detail-oriented, design-driven and passionate developer. I thrive in building scalable
+                    web applications using modern frontend technologies, integrating complex systems and leveragings
+                    modern development tools and practices.</p>
+                <NuxtLink to="/about"
+                    class="text-sky-500 underline flex gap-1 items-center justify-center size-fit leading-0 transition-all duration-200 mt-4">
+                    <span class="">More About Me</span>
+                    <ArrowUpRight :size="16" />
+                </NuxtLink>
+            </div>
         </div>
     </section>
 </template>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>

dca3e938fb5fa497daa056544f147f1e1fece6f0
suleiman hamza <hamzasuleiman530@gmail.com>
2026-06-24 00:47:14 +0100

    style: update layout and background of NewSec component for improved aesthetics

--- app/components/NewSec.vue
+++ app/components/NewSec.vue
@@ -3,10 +3,13 @@
 
 <template>
     <section class="border-b border-indigo-300/30">
-        <div class="container mx-auto px-4 py-8 bg-[radial-gradient(129%_99%_at_112%_85%,_rgb(223,223,250)_20%,_rgb(166,158,255)_90%)] bg-center bg-cover [background-blend-mode: color-dodge] text-black">
-            <p>Forward Thing</p>
-            <p>Feature</p>
-            <p>Forward Thing</p>
+        <div class="bg-[radial-gradient(129%_99%_at_112%_85%,_rgb(113,113,240)_20%,_rgb(90,76,235)_90%)]">
+            <div class="h-70 bg-[url(/images/noise-bg.png)] bg-center bg-contain [background-blend-mode: color-dodge] text-black">
+                <div class="container border p-4">
+                    <p class="font-bold text-2xl">Frontend Engineer</p>
+                    <p class="font-bold text-2xl">Fullstack Engineer</p>
+                </div>
+            </div>
         </div>
     </section>
 </template>

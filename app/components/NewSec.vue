04105ba059c7962386e35b4a803ec0b5409667c8
suleiman hamza <hamzasuleiman530@gmail.com>
2026-06-24 02:01:30 +0100

    Refactor code structure for improved readability and maintainability

--- app/components/NewSec.vue
+++ app/components/NewSec.vue
@@ -5,9 +5,12 @@
     <section class="border-b border-indigo-300/30">
         <div class="bg-[radial-gradient(129%_99%_at_112%_85%,_rgb(113,113,240)_20%,_rgb(90,76,235)_90%)]">
             <div class="h-70 bg-[url(/images/noise-bg.png)] bg-center bg-contain [background-blend-mode: color-dodge] text-black">
-                <div class="container border p-4">
+                <div class="container p-4">
                     <p class="font-bold text-2xl">Frontend Engineer</p>
                     <p class="font-bold text-2xl">Fullstack Engineer</p>
+                    <p class="font-bold text-2xl">Creative Developer</p>
+                    <p class="font-bold text-2xl">ERP Technical Developer</p>
+                    <p class="font-bold text-2xl">Chef</p>
                 </div>
             </div>
         </div>
